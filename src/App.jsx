a2f19ee877f41f6b9f04a1fe4578959e060d59d7
Mugdhashrivastava <shrivastavamugdha19@gmail.com>
2024-01-02 17:50:44 +0530

    plz get committed

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Hero from "./component/Hero";
+import "./App.css";
+
+import Navbar from "./component/Navbar";
+
+const App = () => {
+  return (
+    <div>
+<Navbar />
+
+
+<Hero />
+
+
+    </div>
+  );
+};
+
+export default App;
